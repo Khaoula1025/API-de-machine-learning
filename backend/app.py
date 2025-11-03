ba0fad1378f9ea1a29b068bad50fcd07c1fa8cc5
KarimaChami <karimachami24@gmail.com>
2025-11-03 16:14:37 +0100

    Add coments

--- backend/app.py
+++ backend/app.py
@@ -5,21 +5,39 @@ from config import SessionLocal, engine
 from models import Base, Patient
 import joblib
 import pandas as pd
-# Créer les tables dans la DB au lancement
+# Crée dans la base de données toutes les tables définies dans mes modèles SQLAlchemy (dans models.py) si elles n’existent pas encore.”
 Base.metadata.create_all(bind=engine)
-app = FastAPI()
+# Initialiser l'application FastAPI
+app = FastAPI() 
 
 # Dépendance pour la session DB
+"""
+Chaque fois qu’un utilisateur fait une requête à ton API :
+FastAPI appellera get_db()
+Cela crée une nouvelle connexion à ta base de données
+Puis ton route reçoit cette session dans db: Session = Depends(get_db)
+"""
 def get_db():
     db = SessionLocal() #Ouvre une nouvelle session
     return db
 
 
-# Endpoint de prédiction 
+# Route de prédiction avec le modèle ML
+"""
+Ce qui se passe :
+Tu envoies un JSON de patient (âge, pression, glucose, etc.)
+L’API transforme ce JSON en objet PatientCreate (validation automatique).
+pandas le convertit en DataFrame (format accepté par ton modèle ML).
+Le modèle prédictif (chargé avec joblib) fait une prédiction :
+1 = Risque détecté
+0 = Aucun risque
+"""
+
+
 @app.post("/predict")
 def predict(patient: PatientCreate , db:Session=Depends(get_db)):
     model = joblib.load("cardio_model.pkl")
-    df = pd.DataFrame([patient.__dict__]) # The API transforms this JSON → DataFrame → Model input
+    df = pd.DataFrame([patient.__dict__]) # The API transforms this JSON → patient.__dict__ transforme ton objet Python en dictionnaire puis pandas le transforme en DataFrame → Model input
     prediction = model.predict(df)[0] # the model outputs either "positive" or "negative"
     message = (
         "Risque cardiovasculaire détecté"
@@ -33,11 +51,20 @@ def predict(patient: PatientCreate , db:Session=Depends(get_db)):
 def home():
     return{"message":"Bonjour"}
 
-@app.get("/patients",response_model=list[PatientResponse])
-def get_patients(db: Session = Depends(get_db)):
-    return db.query(Patient).all()
 
-@app.post("/patients",response_model=PatientResponse)
+# Route pour lister les patients
+@app.get("/patients",response_model=list[PatientResponse]) #response_model=list[PatientResponse] → parametre ndemno la réponse sera proprement formatée selon le schéma
+def get_patients(db: Session = Depends(get_db)):
+    return db.query(Patient).all() # une liste d’objets SQLAlchemy/query ncpesiw model:la table lli bghit njib mnha les patients
+
+#Route pour ajouter un nouveau patient
+"""
+Reçoit un JSON de patient
+Crée un objet Patient pour la base de données
+L’enregistre dans la table (db.add, db.commit)
+Retourne le patient ajouté (avec son id généré automatiquement)
+"""
+@app.post("/patients",response_model=PatientResponse) #response_model=PatientResponse → la réponse sera formatée selon le schéma PatientResponse
 def create_patient(patient:PatientCreate, db: Session = Depends(get_db)):
     new_patient = Patient(**patient.dict()) #Crée un objet Patient avec toutes les données reçues automatiquement
     db.add(new_patient) #Prépare l'ajout dans la DB/Ajoute le patient à la session (en attente d’être sauvegardé)
--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./patients.db" # type de base de données et chemin vers le fichier DB
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}) 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
